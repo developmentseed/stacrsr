f1da4ace7c8127bfbfc1973fdaeabb78cea098f4
Alex Mandel <tech@wildintellect.com>
2025-02-19 13:36:03 -0800

    tinker: guessing our way around

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -11,15 +11,22 @@ fn hello_world() -> &'static str {
 #[extendr]
 fn read(
     href: String,
-    //format: Option<String>,
+    format: Option<String>,
     //options: Option<Vec<(String, String)>>,
-) -> String {
-    //let format = format
-    //    .and_then(|f| f.parse::<Format>().ok())
-    //    .or_else(|| Format::infer_from_href(&href))
-    //    .unwrap_or_default();
-    //let options = options.unwrap_or_default();
-    href.to_string()
+) -> Result<Robj> {
+    let format = format
+        .and_then(|f| f.parse::<Format>().ok())
+        .or_else(|| Format::infer_from_href(&href))
+        .unwrap_or_default();
+    let options = options.unwrap_or_default();
+    tokio::future_into_py(py, async move {
+        let value = format
+            .get_opts::<Value, _, _, _>(href, options)
+            .await
+            .map_err(Error::from)?;
+        Ok(Json(value))
+    })
+
 }
 
 // Macro to generate exports.
