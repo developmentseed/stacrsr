ae26d04aba8c24f817d2e9cb1432c3dc8b0a348d
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-02-19 16:41:21 -0500

    tinker: Try to parse STAC JSON using tokio multi-thread runtime
    Trying to add tokio multi-thread runtime to handle async part of the STAC JSON parsing.

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,5 +1,10 @@
 use extendr_api::prelude::*;
-use stac::{Format, Value};
+use extendr_api::serializer::to_robj;
+use extendr_api::ToVectorValue;
+use serde;
+use stac::mime::JSON;
+use stac::{Error, Format, Value};
+use tokio;
 
 /// Return string `"Hello world!"` to R.
 /// @export
@@ -8,25 +13,35 @@ fn hello_world() -> &'static str {
     "Hello world!"
 }
 
+struct Json<T: serde::Serialize>(T);
+
 #[extendr]
-fn read(
+pub fn read(
     href: String,
     format: Option<String>,
-    //options: Option<Vec<(String, String)>>,
-) -> Result<Robj> {
+    options: Option<Vec<(String, String)>>,
+) -> Result<Json<Value>> {
+    //Result<Bound<'_, Any>> {
     let format = format
         .and_then(|f| f.parse::<Format>().ok())
         .or_else(|| Format::infer_from_href(&href))
         .unwrap_or_default();
     let options = options.unwrap_or_default();
-    tokio::future_into_py(py, async move {
+
+    // Initialize async runtime
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    runtime.block_on(async {
         let value = format
             .get_opts::<Value, _, _, _>(href, options)
             .await
-            .map_err(Error::from)?;
-        Ok(Json(value))
+            .map_err(Error::from)
+            .unwrap();
+        Ok(JSON(value))
     })
-
 }
 
 // Macro to generate exports.
